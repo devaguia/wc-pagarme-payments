e64bfa724e6eabf2a1f2235e71c5009d6d45c3ec
Matheus Aguiar <aguiartgv@gmail.com>
2022-09-30 15:43:09 -0300

    feat: add variables to view files

--- app/Controllers/Menus/Pagarme.php
+++ app/Controllers/Menus/Pagarme.php
@@ -3,6 +3,7 @@
 namespace WPP\Controllers\Menus;
 
 use WPP\Controllers\Render\Render;
+use WPP\Model\Repository\Settings;
 
 /**
  * Name: Pagarme
@@ -12,6 +13,16 @@ use WPP\Controllers\Render\Render;
 class Pagarme extends Render
 {
     /**
+     * @var array
+     */
+    private $fields;
+
+    public function __construct()
+    {
+        $this->get_fields();
+    }
+
+    /**
      * Enqueue custom scripts and styles to the page
      * @return void
      */
@@ -20,6 +31,65 @@ class Pagarme extends Render
         $this->enqueue_scripts( [ 'name' => 'wpp-admin', 'file' => 'scripts/admin/pages/pagarme/index.js' ] );
         $this->enqueue_styles( [ 'name' => 'wpp-admin', 'file' => 'styles/admin/pages/pagarme/index.css' ] );
     }
+
+    /**
+     * 
+     * @return void
+     */
+    private function default()
+    {
+        $this->fields = [
+            'production_key'      => '',
+            'test_key'            => '',
+            'methods'             => [],
+            'credit_installments' => [],
+            'anti_fraud'          => false,
+            'anti_fraud_value'    => 0,
+            'success_status'      => 'wc-processing',
+            'order_logs'          => false,
+            'api_version'         => 1,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function get_fields()
+    {
+        $settings = new Settings;
+        $fields = $settings->find();
+
+        $this->default();
+
+        foreach ( $fields as $field ) {
+            if ( isset( $field->key ) && isset( $field->value ) ) {
+                if ( isset( $this->fields[$field->key] ) && $field->value ) {
+                    $this->fields[$field->key] = $this->verify( $field->value );
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if variable is serialized and unserialize
+     * @return mixed
+     */
+    private function verify( $value )
+    {
+        if( is_serialized( $value ) ) {
+            return unserialize( $value );
+        }
+
+        if ( $value === 'on' || $value === 'off' ) {
+            return $value === 'on' ? true : false;
+        }
+
+        if ( is_numeric( $value ) ) {
+            return intval( $value );
+        }
+
+        return $value;
+    }
     
     /**
      * Call the view render
@@ -27,7 +97,7 @@ class Pagarme extends Render
      */
     public function request()
     {
-        $this->render( 'Admin/settings/pagarme.php', [] );
+        $this->render( 'Admin/settings/pagarme.php', $this->fields );
         $this->enqueue();
     }
 }
--- app/Views/Admin/settings/pagarme.php
+++ app/Views/Admin/settings/pagarme.php
@@ -1,3 +1,41 @@
+<?php 
+/**
+ * Available variables
+ * 
+ * @var string $production_key 
+ * @var string $test_key 
+ * @var array $methods 
+ * @var array $credit_installments 
+ * @var array $anti_fraud 
+ * @var string $anti_fraud_value 
+ * @var string $success_status 
+ * @var bool $order_logs 
+ * @var int $api_version 
+ */
+
+ $active_class  = "fa-solid fa-circle-check";
+ $disable_class = "fa-solid fa-circle-xmark";
+
+ $methods = [
+     'wc-pagarme-billet' => [
+         'active' => true,
+         'label'  => 'Bank Slip',
+         'mode'   => 'sandbox'
+     ],
+     'wc-pagarme-credit' => [
+        'active' => true,
+        'label'  => 'Credit Card',
+        'mode'   => 'sandbox'
+     ],
+     'wc-pagarme-pix' => [
+        'active' => true,
+        'label'  => 'Pix',
+        'mode'   => 'production'
+     ]
+ ];
+
+?>
+
 <div class="wrap wpp-wrap">
     <div class="wpp-container wpp-container-pagarme">
         <div class="title">
@@ -10,99 +48,49 @@
                 <div>
                     <h3><? echo __( "Allowed Methods:", "wc-pagarme-payments" ); ?></h3>
                 </div>
-                <div class="methods">
-                    <div>
-                        <div class="method">
-                            <div class="status status-disabled">
-                                <i class="fa-solid fa-circle-xmark"></i>
+                <?php if ( isset( $methods ) && is_array( $methods ) ) :?>
+                    <div class="methods">
+                        <?php foreach( $methods as $key => $method ): ?>
+                            <div class="method">
+                                <div class="icon">
+                                    <?php $activate = isset( $method['active'] ) && $method['active'] ? true : false; ?>
+                                    <div class="status status-<?php echo esc_attr( $activate ? 'activate' : 'disabled' ); ?>">
+                                        <i class="<?php echo esc_attr( $activate ? $active_class : $disable_class ); ?>"></i>
+                                    </div>
+                                </div>
+                                <div class="label">
+                                    <span> <?php echo esc_html( isset( $method['label'] ) ? $method['label'] : '' ); ?></span>
+                                </div>
+                                <div class="mode">
+                                    <span><?php echo esc_html( isset( $method['mode'] ) ? $method['mode'] : '' ); ?></span>
+                                </div>
+                                <div class="link">
+                                    <a target="_blank" href="admin.php?page=wc-settings&tab=checkout&section=<?php echo esc_html( $key ) ?>">
+                                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                                    </a>
+                                </div>
                             </div>
-                            <div class="label">
-                                <span> <?php echo __( "Bank Slip", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="status status-disabled">
-                                <i class="fa-solid fa-circle-xmark"></i>
-                            </div>
-                            <div class="label">
-                                <span> <?php echo __( "Credit Card", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="status status-activate">
-                                <i class="fa-solid fa-circle-check"></i>
-                            </div>
-                            <div class="label">
-                                <span> <?php echo __( "Pix", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="method">
-                            <div class="mode">
-                                <span><?php echo __( "sanbox", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="mode">
-                                <span><?php echo __( "sanbox", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="mode">
-                                <span><?php echo __( "production", "wc-pagarme-payments" ); ?></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="method">
-                            <div class="link">
-                                <a target="_blank" href="admin.php?page=wc-settings&tab=checkout&section=wc-pagarme-billet">
-                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="link">
-                                <a target="_blank" href="admin.php?page=wc-settings&tab=checkout&section=wc-pagarme-credit">
-                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="method">
-                            <div class="link">
-                                <a target="_blank" href="admin.php?page=wc-settings&tab=checkout&section=wc-pagarme-pix">
-                                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        <?php endforeach ?>
+                <?php endif; ?>
             </div>
             <div class="plugin-options">
                 <div class="title">
                     <h3><? echo __( "Plugin Options:", "wc-pagarme-payments" ); ?></h3>
                 </div>
-                <div>
-                    <h4><strong><?php echo __( "Icons", "wc-pagarme-payments" ); ?></strong></h4>
-                    <div>
-                        <input type="checkbox" name="wpp-show-icons" id="wpp-show-icons">
-                        <label for="wpp-show-icons"><?php echo __( "Show Pagar.me icons on checkout.", "wc-pagarme-payments"); ?></label>
-                    </div>
-                </div>
                 <div class="status">
                     <h4><strong><?php echo __( "Status", "wc-pagarme-payments" ); ?></strong></h4>
                     <div>
                         <label for=""><?php echo __( "Status for completed orders:", "wc-pagarme-payments" ); ?></label>
-                        <select name="wpp-finish-order-status" id="wpp-finish-order-status">
+                        <select name="wpp-finish-order-status" id="wpp-finish-order-status" value="<?php echo esc_html( isset( $success_status ) ? $success_status : 'wc-processing' ); ?>" >
                             <option value="wc-processing">Processing</option>
+                            <option value="wc-completed">Completed</option>
                         </select>
                     </div>
                 </div>
                 <div class="status">
                     <h4><strong><?php echo __( "Order Logs", "wc-pagarme-payments" ); ?></strong></h4>
                     <div>
-                        <input type="checkbox" name="wpp-order-logs" id="wpp-order-logs">
+                        <input type="checkbox" name="wpp-order-logs" id="wpp-order-logs" <?php echo esc_attr( isset( $order_logs ) && $order_logs ? 'checked' : '' ) ?>>
                         <label for="wpp-order-logs"><?php echo __( "Add log section on order edit page.", "wc-pagarme-payments"); ?></label>
                     </div>
                 </div>
@@ -118,7 +106,7 @@
                             <span><?php echo __( "Production secret key:", "wc-pagarme-payments" ); ?></span>
                         </div>
                         <div>
-                            <input type="password" name="wpp-production-secret-key" id="wpp-production-secret-key">
+                            <input type="password" name="wpp-production-secret-key" id="wpp-production-secret-key" value="<?php echo esc_html( isset( $production_key ) ? $production_key : '' ); ?>">
                         </div>
                     </div>
                     <div class="key">
@@ -126,7 +114,7 @@
                             <span><?php echo __( "Test secret key:", "wc-pagarme-payments"  ); ?></span>
                         </div>
                         <div>
-                            <input type="password" name="wpp-test-secret-key" id="wpp-test-secret-key">
+                            <input type="password" name="wpp-test-secret-key" id="wpp-test-secret-key" value="<?php echo esc_html( isset( $test_key ) ? $test_key : '' ); ?>" >
                         </div>
                     </div>
                 </div>
@@ -134,11 +122,11 @@
                     <div class="option">
                         <h4><strong><?php echo __( "Anti Fraud", "wc-pagarme-payments" ); ?></strong></h4>
                         <div>
-                            <input type="checkbox" name="wpp-anti-fraud" id="wpp-wpp-anti-fraud">
+                            <input type="checkbox" name="wpp-anti-fraud" id="wpp-wpp-anti-fraud" <?php echo esc_attr( isset( $anti_fraud ) && $anti_fraud ? 'checked' : '' ) ?>>
                             <label for="wpp-anti-fraud"><?php echo __( "Enable anti-fraud.", "wc-pagarme-payments"); ?></label>
                             <div class="anti-fraud">
                                 <label for=""><?php echo __( "Minimum value:", "wc-pagarme-payments"); ?></label>
-                                <input type="text" name="wpp-anti-fraud-value" id="wpp-anti-fraud-value">
+                                <input type="text" name="wpp-anti-fraud-value" id="wpp-anti-fraud-value" value="<?php echo esc_html( isset( $anti_fraud_value ) ? $anti_fraud_value : '' ); ?>">
                             </div>
                         </div>
                     </div>
@@ -147,7 +135,7 @@
                         <div>
                             <label for="wpp-pagarme-api-version"><?php echo __( "Pagar.me API version:", "wc-pagarme-payments" ); ?></label>
                             <div>
-                                <select name="wpp-pagarme-api-version" id="wpp-pagarme-api-version">
+                                <select name="wpp-pagarme-api-version" id="wpp-pagarme-api-version" value="<?php echo esc_html( isset( $api_version ) ? $api_version : 1 ); ?>" >
                                     <option value="1">Versão 1</option>
                                     <option value="2">Versão 2</option>
                                 </select>
