750e92a02cdcd24cfc53d56500c816409b98f0db
Matheus Aguiar <aguiartgv@gmail.com>
2023-03-03 09:48:21 -0300

    build: removing plugins files

--- app/Views/Pages/checkout/billet.php
+++
@@ -1,6 +0,0 @@
-<?php use WPP\Helpers\Config; ?>
-<div class="wpp-payment-container wpp-billet-checkout">
-    <div class="barcode-image">
-        <img src="<?php echo Config::__images( "barcode.png" )?>" alt="Barcode icon">
-    </div>
-</div>
